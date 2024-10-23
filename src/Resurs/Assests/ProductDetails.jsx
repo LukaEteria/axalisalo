0e6ee31016de8454f03af2b203babf185d72b5ac
Lukaeteria <eterialuka650@gmail.com>
2024-10-24 03:05:11 +0400

    Initial commit

--- src/Resurs/Assests/ProductDetails.jsx
+++ src/Resurs/Assests/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import products from '../assets/products';
+import products from "../../assets/products";
 
 
 const ProductDetails = () => {
